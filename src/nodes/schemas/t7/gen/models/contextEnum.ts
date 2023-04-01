0a93305d01935153c0f491f13d56c346f4413480
Sean-Meyers <smmeyers@ualberta.ca>
2023-03-31 19:09:16 -0600

    dumb casing thing again. temporary rename so i don't get a reset loop

--- src/nodes/schemas/t7/gen/models/contextEnum.ts
+++
@@ -1,7 +0,0 @@
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-
-export enum contextEnum {
-    HTTPS_WWW_W3_ORG_NS_ACTIVITYSTREAMS = 'https://www.w3.org/ns/activitystreams',
-}
